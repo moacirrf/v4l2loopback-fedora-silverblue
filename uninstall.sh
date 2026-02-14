8c4d8e5c7992ac34e30c4fe41e993df1d505f993
Moacir da Roza Flores <moacir.flores@mobsolution.com.br>
2026-02-14 14:08:28 -0300

    install.sh: add fallback systemd unit for v4l2loopback
    This change introduces a second systemd unit,
    `v4l2loopback-silverblue-failure.service`, which rebuilds the
    v4l2loopback module if `v4l2loopback-silverblue.service` fails
    to load the driver.
    
    This is useful after system updates when the module has not yet
    been rebuilt, preventing driver load failures.
    
    	modified:   install.sh
    	modified:   uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,8 @@
 echo "Removing service..."
 systemctl disable v4l2loopback-silverblue
 rm /etc/systemd/system/v4l2loopback-silverblue.service
+rm /etc/systemd/system/v4l2loopback-silverblue-failure.service
+
 echo "Unloading module v4l2loopback"
 rmmod `pwd`/build/v4l2loopback/v4l2loopback.ko
 echo "Ok.Bye bye"
